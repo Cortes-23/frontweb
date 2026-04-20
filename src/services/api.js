9dcbaefa2b7e4f6143f912101b5501f2f5931421
David Cortes <steveen.cortess@gmail.com>
2026-04-20 16:16:53 -0500

    Cambio de raiz

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "frontweb-5waztyscq-cortes-23s-projects.vercel.app"
+  baseURL: import.meta.env.VITE_API_URL || "https://backend-iwr8.onrender.com/api"
 })
 
 api.interceptors.request.use((config) => {
